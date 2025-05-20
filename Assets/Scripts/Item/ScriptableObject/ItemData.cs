6d1be0fe64b62e018c66b9b6c88d66b6219a5695
jwoongA <leobyun17@gmail.com>
2025-05-20 17:44:34 +0900

    feat : 아이템 설명추가를 위한 중간 세이브
    아이템 설명추가를 위한 중간 세이브

---
+++ Assets/Scripts/Item/ScriptableObject/ItemData.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    SpeedBoost,
+    JumpBoost,
+    HealthPotion
+}
+
+[SerializeField]
+public class ItemDataConsumable
+{
+    public ItemType _itemType;
+    public float value;
+}
+
+[CreateAssetMenu(fileName = "Item", menuName = "New Item")]
+public class ItemData : ScriptableObject
+{
+    [Header("info")]
+    public string displayName;
+    public string description;
+    public ItemType type;
+    public GameObject dropPrefab;
+    
+    [Header("Consumable")]
+    public ItemDataConsumable[] consumables;
+}
